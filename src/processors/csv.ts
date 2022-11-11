35141863cb5277d0566212891651b555269fb4c9
christopher-caldwell <well.done.514@gmail.com>
2022-11-10 21:11:16 -0600

    fix: Making parsing libs optional imports if you use them

--- src/processors/csv.ts
+++ src/processors/csv.ts
@@ -1,8 +1,7 @@
-import { parse } from 'csv-parse/sync'
-
 import { LoggerOverride } from '@/index'
 
-export const csvProcessor = <TInput>(data: unknown, logger?: LoggerOverride): TInput => {
+export const csvProcessor = async <TInput>(data: unknown, logger?: LoggerOverride): Promise<TInput> => {
+  const { parse } = await import('csv-parse/sync')
   const isDataValidForParsing = typeof data === 'string' || Buffer.isBuffer(data)
   if (!isDataValidForParsing) throw new Error('[csvProcessor]: Provided data is not compatible to be parsed as CSV')
   const result = parse(data, { columns: true }) as TInput
